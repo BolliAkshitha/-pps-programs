c01c58018d1179bb9097408e54868f843456b73a
BolliAkshitha <85949713+BolliAkshitha@users.noreply.github.com>
2021-07-21 12:16:43 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int l,b,a,r,p;
+	printf("\n enter l:");
+	scanf("%d",&l);
+	printf("\n enter b:");
+	scanf("%d",&b);
+	a = (l*b);
+	printf("%d",a);
+	p = (2*l+b);
+	printf("%d",p);
+	printf("\n enter r:");
+	scanf("%d",&r);
+	a=(2*3.14*r);
+	printf("%d",a);
+	return 0;
+}
